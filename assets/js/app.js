9da21e5dc53902715fd33170e13e12e24555f307
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-07-27 18:27:34 -0500

    Got the profile information to show up on the page. Working on getting the repo information to show up.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,27 @@
-// stuff
+(function() {
+  var profileTemplate = $('#profile-temp').html();
+  var repoTemplate = $('#repo-temp').html();
+
+  var p = Handlebars.compile(profileTemplate);
+  var r = Handlebars.compile(repoTemplate);
+
+  $.ajax({
+  	url: 'https://api.github.com/users/jacklynlee31',
+  	dataType: 'json',
+		success: function(data) {
+			var ht = p(data);
+			$('.profile').html(ht);
+		}
+	});
+
+  $.ajax({
+  	url: 'https://api.github.com/users/jacklynlee31/repos',
+  	dataType: 'json',
+		success: function(data) {
+			var ht = r(data);
+			$('.repo').html(ht);
+		}
+	});
+
+
+})();
