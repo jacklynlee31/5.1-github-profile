6577674973c798cd97007946c1421cd3be968576
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-08-08 21:21:41 -0500

    Attempt 1

---
+++ app.js
@@ -0,0 +1,25 @@
+(function() {
+  var profileTemplate = $('#profile-temp').html();
+  var repoTemplate = $('#repo-temp').html();
+
+  var p = Handlebars.compile(profileTemplate);
+  var r = Handlebars.compile(repoTemplate);
+
+  var apiUrl = 'https://api.github.com/users/jacklynlee31';
+
+  $.ajax({
+  url: apiUrl,
+  success: function(data) {
+    var ht = p(data);
+    $('.profile').html(ht);
+  }
+	});
+
+  $.ajax ({
+    url: 'https://api.github.com/users/jacklynlee31/repos',
+    success: function(data) {
+      var results = r(data);
+      $('.repo').html(results);
+    }
+  });
+})();
